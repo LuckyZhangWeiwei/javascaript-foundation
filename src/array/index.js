b46573d5e1c6bba38075956fb209b79d343217fc
weiwei zhang <zhangzhang_10@hotmail.com>
2021-06-04 10:19:48 +0800

    arry method

--- src/array/index.js
+++ src/array/index.js
@@ -63,51 +63,89 @@
 // return 以新的数组返回， 原数组不变
 {
   let arr = [1, 2, 3, 4, 5, 6];
-//   let res = arr.slice(1,3)
-//   console.log(res, arr)
+  //   let res = arr.slice(1,3)
+  //   console.log(res, arr)
 
-//   res = arr.slice(1)
-//   console.log(res, arr)
+  //   res = arr.slice(1)
+  //   console.log(res, arr)
 
-//   res = arr.slice(0) // 等于数组克隆
-//   console.log(res, arr)
+  //   res = arr.slice(0) // 等于数组克隆
+  //   console.log(res, arr)
 
-//   res = arr.slice() // 等于数组克隆
-//   console.log(res, arr)
+  //   res = arr.slice() // 等于数组克隆
+  //   console.log(res, arr)
 
-// let res = arr.slice(-1, -2)
-// console.log(res, arr)
+  // let res = arr.slice(-1, -2)
+  // console.log(res, arr)
 
-// res =arr.slice(2,1)
-// console.log(res, arr)
+  // res =arr.slice(2,1)
+  // console.log(res, arr)
 
-res = arr.slice(2.8, 3)
-console.log(res, arr)
+  res = arr.slice(2.8, 3);
+  console.log(res, arr);
 }
 
 //concat 数组拼接
 // params 多个任意值
 // return 拼接后的新数组（原数组不变）
 {
-	let ary1 = [1,2,3]
-	let ary2 = [4,5,6]
-	// let res = ary1.concat("p")
-	// console.log(res, ary1)
+  let ary1 = [1, 2, 3];
+  let ary2 = [4, 5, 6];
+  // let res = ary1.concat("p")
+  // console.log(res, ary1)
 
-    // let res = ary1.concat() // 数组的克隆
-	// console.log(res, ary1, res === ary1)
+  // let res = ary1.concat() // 数组的克隆
+  // console.log(res, ary1, res === ary1)
 
-	let res = ary1.concat("p", ary2)
-	console.log(res, ary1)
+  let res = ary1.concat("p", ary2);
+  console.log(res, ary1);
 }
 
 //toString, 把数组转化为字符串 原数组不变
 // params
 // return 转化后的字符串（原数组不变）
 {
-	let ary = [1,2,3]
-	let res = ary.toString()
-	console.log(res, [1,2,3].join("|"))
+  let ary = [1, 2, 3];
+  let res = ary.toString();
+  console.log(res, [1, 2, 3].join("|"));
 
-	console.log({}.toString.call(ary))
+  console.log({}.toString.call(ary));
+  console.log(eval([1, 2, 3].join("+")));
+}
+/***************检测数组中是否包含某项**************************/
+// indexof / lastIndexOf 检测当前项在数组中第一次或者最后一次出现的位置
+// params 要检索的这一项内容
+// return 这一项出现的位置索引（数字）， 如果没有这一项，则返回-1
+// comment: 原数组不变，在ie6-8 不兼容
+{
+  let ary = [1, 2, 3, 1, 2, 3];
+  console.log(ary.indexOf(2), ary.lastIndexOf(2));
+  console.log(ary.includes(2)); // es6
+}
+/***************数组排序排列******************************************/
+// reverse 把数组倒过来 (注意不是从小到大)
+// param
+// return 排列后的新数组   原数组改变
+{
+  let ary = [1, 2, 3, 1, 2, 3];
+  console.log(ary.reverse(), ary);
+  console.log(ary.reverse() === ary);
+}
+// sort
+// param 可有可无
+// return 排序后的新数组， 原数组改变
+{
+  let ary = [1, 2, 3, 1, 2, 3];
+  let res = ary.sort();
+  console.log(ary, res, ary === res);
+  console.log("------------------------");
+  ary = [12, 15, 9, 28, 10, 22]; // 按照第一个字符排序， sort 方法如果不传参，无法处理10 以上的数字排序的
+  // res = ary.sort();
+  // console.log(ary);
+  console.log("--------------------------");
+  ary.sort((a, b) => {
+    console.log(a, b)
+    return a-b
+  });
+  console.log(ary)
 }
