21e7dc538897a7435662cffb0dd7a0678feea9c4
weiwei zhang <zhangzhang_10@hotmail.com>
2021-06-09 14:01:42 +0800

    change

--- src/redux-learning/test.js
+++ src/redux-learning/test.js
@@ -3,7 +3,7 @@ function fun1() {
 }
 
 function b(next) {
-	console.log(next.toString())
+	// console.log(next.toString())
 	return function() {
 		console.log("b")
 		next()
@@ -26,7 +26,7 @@ b(fun1)
 
 
 
-// a(b(fun1))
+a(b(fun1))
 
 // function() {
 // 	console.log("a")
@@ -34,6 +34,6 @@ b(fun1)
 // 	console.log("fun1")
 // }
 
-// a(b(fun1))()
+a(b(fun1))()
 
 
