05e72573c9f8122a6efbfdd761c562498f39909c
zhangweiwei <zhangzhang_10@hotmail.com>
2021-07-07 19:06:01 +0800

    change

--- src/p22-阿里的一道引发血案的面试题/index.js
+++ src/p22-阿里的一道引发血案的面试题/index.js
@@ -4,10 +4,19 @@
   };
   let b = a;
 
+  // 连等 相当于 a.x = {n:2}   a = {n:2}
   a.x = a = {
     n: 2,
   };
 
   console.log(a.x);
+  console.log(b);
+}
+{
+  // 死循环
+  let a = { n: 1 };
+  let b = a;
+  a.x = b;
+  console.log(a);
   console.log(b);
 }
