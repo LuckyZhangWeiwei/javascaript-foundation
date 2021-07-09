88b3fffd7719f6b8de8b4e4a0c5eced99d5ba580
zhangweiwei <zhangzhang_10@hotmail.com>
2021-07-09 11:42:56 +0800

    change

---
+++ src/p89-变量提升机制/index.js
@@ -0,0 +1,15 @@
+{
+  console.log(a);
+  var a = 12;
+  var b = a;
+  b = 13;
+  console.log(a);
+  console.log(b);
+}
+
+{
+  console.log(sum(10, 20));
+  function sum(n, m) {
+    return n + m;
+  }
+}
