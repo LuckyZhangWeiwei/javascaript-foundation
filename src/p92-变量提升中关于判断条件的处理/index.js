1f58ff7c9f3fcfb73de05b1849eb7b8d92df899a
zhangweiwei <zhangzhang_10@hotmail.com>
2021-07-10 15:35:01 +0800

    change

--- src/p92-变量提升中关于判断条件的处理/index.js
+++ src/p92-变量提升中关于判断条件的处理/index.js
@@ -19,3 +19,27 @@
   }
   fn();
 }
+
+{
+  f = function () {
+    return true;
+  };
+  g = function () {
+    return false;
+  };
+  ~(function () {
+    // 函数自执行会形成一个私有的作用域
+    // 变量提升 function g;
+    // 满足if 条件后才会 声明fn （开一个堆内存），但是在if的时候就已经执行g(), 所以会报错
+    if (g() && [] == ![]) {
+      f = function () {
+        return false;
+      };
+      function g() {
+        return true;
+      }
+    }
+  })();
+  console.log(f());
+  console.log(g());
+}
