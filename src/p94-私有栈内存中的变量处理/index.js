6c5f020bf8b1d3e4e9082eb47be36748e0e758c3
zhangweiwei <zhangzhang_10@hotmail.com>
2021-07-12 12:04:12 +0800

    change

--- src/p94-私有栈内存中的变量处理/index.js
+++ src/p94-私有栈内存中的变量处理/index.js
@@ -10,3 +10,20 @@
   fn();
   console.log(a, b);
 }
+
+{
+  console.log(a, b, c);
+  var a = 12,
+    b = 13,
+    c = 14;
+
+  function fn(a) {
+    console.log(a, b, c);
+    a = 100;
+    c = 200;
+    console.log(a, b, c);
+  }
+
+  b = fn(10);
+  console.log(a, b, c);
+}
