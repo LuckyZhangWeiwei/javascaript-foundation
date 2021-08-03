2db5b45036771385e53a4893bcfef09c88665b35
zhangweiwei <zhangzhang_10@hotmail.com>
2021-08-03 11:11:52 +0800

    change

--- src/珠峰-函数式编程/14.js
+++ src/珠峰-函数式编程/14.js
@@ -1,11 +1,34 @@
-class Functor {
+// 单子 Monad
+class Monad {
   constructor(value) {
     this.value = value;
   }
   static of(value) {
-    return new Functor(value);
+    return new Monad(value);
   }
   map(fn) {
-    return new Functor(fn(this.value));
+    // console.log(fn.toString(), typeof this.value);
+    return new Monad(fn(this.value));
+  }
+  join() {
+    return this.value;
+  }
+  flatMap(fn) {
+    // 本身是函子的值也是一个函子，把值取出来
+    return this.map(fn).join();
   }
 }
+
+let result = Monad.of("hello")
+  .map((x) => Monad.of(x + 1))
+  .map((x) => Monad.of(x + 2))
+  .map((x) => Monad.of(x + 3));
+
+console.log(result);
+
+let result2 = Monad.of("hello")
+  .flatMap((x) => Monad.of(x + 1))
+  .flatMap((x) => Monad.of(x + 2))
+  .flatMap((x) => Monad.of(x + 3));
+
+console.log(result2);
